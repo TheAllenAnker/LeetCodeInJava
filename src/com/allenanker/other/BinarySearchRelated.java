d25ac8a07245e2a2d44fa827b4c809476dd4a473
TheAllenAnker <theallenanker@gmail.com>
2018-10-30 20:04:50 +0800

    Binary search related problem: Find the last target element in a sorted array.

--- src/com/allenanker/other/BinarySearchRelated.java
+++ src/com/allenanker/other/BinarySearchRelated.java
@@ -4,7 +4,7 @@ public class BinarySearchRelated {
 
     public static void main(String[] args) {
         int[] nums = new int[]{1, 3, 4, 5, 6, 8, 8, 8, 11, 18};
-        System.out.println(findFirst2(nums, 8));
+        System.out.println(findLast1(nums, 8));
     }
 
     /**
@@ -48,6 +48,23 @@ public class BinarySearchRelated {
             } else {
                 if (mid == 0 || nums[mid - 1] != target) return mid;
                 else right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int findLast1(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                if (mid == nums.length - 1 || nums[mid + 1] != target) return mid;
+                else left = mid + 1;
             }
         }
 
