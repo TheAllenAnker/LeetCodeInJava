40223b4893cc6d49af6342eb5c4b5325a0ada07a
TheAllenAnker <theallenanker@gmail.com>
2018-11-03 15:22:07 +0800

    Shuffle an Array.

---
+++ src/com/allenanker/topinterviewed/ShuffleAnArray.java
@@ -0,0 +1,34 @@
+package com.allenanker.topinterviewed;
+
+import java.util.Random;
+
+public class ShuffleAnArray {
+
+    private int[] nums;
+    private Random random;
+
+    public ShuffleAnArray(int[] nums) {
+        this.nums = nums;
+        random = new Random();
+    }
+
+    public int[] reset() {
+        return nums;
+    }
+
+    public int[] shuffle() {
+        if (nums == null) return null;
+        int[] a = nums.clone();
+        for (int j = 1; j < a.length; j++) {
+            int i = random.nextInt(j + 1);
+            swap(a, i, j);
+        }
+        return a;
+    }
+
+    private void swap(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+}
