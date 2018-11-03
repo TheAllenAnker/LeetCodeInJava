3e22bbafed6fa51d98d3b85f87ea36a5e20c0519
TheAllenAnker <theallenanker@gmail.com>
2018-11-03 19:37:25 +0800

    First Unique Character in a String.

--- src/com/allenanker/topinterviewed/TopInterviewed.java
+++ src/com/allenanker/topinterviewed/TopInterviewed.java
@@ -1,8 +1,13 @@
 package com.allenanker.topinterviewed;
 
+import java.util.LinkedHashMap;
+import java.util.SortedMap;
+import java.util.SortedSet;
+
 public class TopInterviewed {
     public static void main(String[] args) {
-        System.out.println(sumOfIntegers(1, 2));
+//        System.out.println(sumOfIntegers(1, 2));
+        System.out.println(firstUniqChar("loveleetcode"));
     }
 
     public static ListNode detectCycle(ListNode head) {
@@ -35,6 +40,39 @@ public class TopInterviewed {
         }
 
         return a;
+    }
+
+    /**
+     * Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+     * Note: You may assume the string contain only lowercase letters.
+     *
+     * @param s input string
+     * @return first unique char's index
+     */
+    public static int firstUniqChar(String s) {
+        LinkedHashMap<Character, Integer> chars = new LinkedHashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (chars.containsKey(c)) {
+                chars.put(c, chars.get(c) + 1);
+            } else {
+                chars.put(c, 1);
+            }
+        }
+        char target = '*';
+        for (char c : chars.keySet()) {
+            if (chars.get(c) == 1) {
+                target = c;
+                break;
+            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == target) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
 
