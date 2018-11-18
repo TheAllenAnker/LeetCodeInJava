26bdaeccac503462562a4ce31a39512fba72e106
TheAllenAnker <theallenanker@gmail.com>
2018-11-18 21:11:57 +0800

    In-order Traversal. Not recursively.

--- src/com/allenanker/tree/TreeRelated.java
+++ src/com/allenanker/tree/TreeRelated.java
@@ -5,11 +5,42 @@ import java.util.Stack;
 public class TreeRelated {
     public static void main(String[] args) {
         preOrderTraversal(generateSimpleTree());
+        inOrderTraversal(generateSimpleTree());
+    }
+
+    /**
+     * Traversal a binary tree in in-order. Not recursively.
+     *
+     * @param root root of the binary tree
+     */
+    public static void inOrderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.println("In-order Traversal:");
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (!stack.isEmpty() || curr != null) {
+            // push all the left nodes into the stack first
+            if (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            } else {
+                // reach the end of the left subtree, the parent of this end should be pop out
+                // and the right subtree of this parent should be handled in the above way
+                curr = stack.pop();
+                System.out.print(curr.val + "->");
+                curr = curr.right;
+            }
+        }
+        System.out.print("end\n");
     }
 
     /**
      * Traversal a binary tree in pre-order. Not recursively.
-     * @param root root of binary tree
+     *
+     * @param root root of the binary tree
      */
     public static void preOrderTraversal(TreeNode root) {
         if (root == null) {
